090bde210e0762d9aebb47b243a5b1bda6d04116
Mohsen Pourdehghan <mohsen.purdehghan@gmail.com>
2024-08-31 18:36:14 +0330

    upload file

---
+++ show_text_with_aski_encrypted.py
@@ -0,0 +1,43 @@
+
+def encrypt(text : str):
+
+    encrypted_text = ''
+
+    for char in text:
+
+        char_aski = ord(char) * 21
+        char_encrypted = chr(char_aski)
+        encrypted_text += char_encrypted
+
+    return encrypted_text
+
+
+
+def decrypt(encrypted_text : str):
+
+    decrypted_text = ''
+
+    for aski in encrypted_text:
+
+        char_aski = ord(aski) // 21 
+        char_decrypted = chr(char_aski)
+        decrypted_text += char_decrypted
+
+    return decrypted_text
+
+
+
+text = 'hello this is a homework for python plus'
+
+enc_text = encrypt(text)
+
+print("encrypted text: ",'\n',enc_text,'\n')
+
+
+user_input = input('enter the algorithm: ')
+
+if user_input.replace(' ', '') == '//21':
+    print("\ndecrypted text: ",'\n',decrypt(enc_text),'\n')
+
+else:
+    print("Incorrect!!")
